c2234fb3c65ff2d7ac6c35e5b866ddeec15a3d18
Leonardo Dionizio <leonardo.dionizio@kovi.com.br>
2024-11-29 16:50:53 -0300

    feat: add layout max width

--- jokenpo-ts/src/components/layout/wrapper/index.tsx
+++ jokenpo-ts/src/components/layout/wrapper/index.tsx
@@ -7,7 +7,7 @@ type WrapperProps = {
 
 export const Wrapper = ({ children }: WrapperProps) => {
   return (
-    <Stack alignItems="center" style={{ width: "100vw" }} padding={8}>
+    <Stack alignItems="center" padding={8} width="full" maxW={600} margin="auto">
       {children}
     </Stack>
   );
